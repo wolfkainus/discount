792f7f66a0c303799c28132957f5a340df7bf320
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-11-30 18:58:03 -0300

    Remove bonu scaffold

--- app/views/bonus/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @bonu, :id, :title, :description, :photo, :value, :expiration, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :coupons
-  resources :bonus
   resources :locations
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
