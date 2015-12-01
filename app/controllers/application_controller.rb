8168c4e8447c0df40b2c87195525c2a7ea5a9761
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-12-01 17:53:19 -0300

    Add field to registration User and Sanitidevise_parameter_sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:name, :last_name, :user_name, :address, :avatar]
+      devise_parameter_sanitizer.for(:account_update) << [:name, :last_name, :user_name, :address, :avatar]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
 	has_many :coupons, dependent: :destroy
 	
 	validates :name, presence: true
-	validates :lastname, presence: true
-	validates :username, presence:true, uniqueness: true
+	validates :last_name, presence: true
+	validates :user_name, presence:true, uniqueness: true
 	validates :email, presence: true
 	validates :address, presence: true
-	validates :photo, presence: true
+	validates :avatar, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
