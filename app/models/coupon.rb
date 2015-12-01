d351fcb9985067bf1cc8d8801d6f9acd51033736
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-12-01 16:51:30 -0300

    Relation between User Coupon and validate field

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,2 +1,8 @@
 class Coupon < ActiveRecord::Base
+	belongs_to :user
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :photo, presence: true
+	validates :value, presence: true
+	validates :expiration, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+	has_many :coupons, dependent: :destroy
+	
+	validates :name, presence: true
+	validates :lastname, presence: true
+	validates :username, presence:true, uniqueness: true
+	validates :email, presence: true
+	validates :address, presence: true
+	validates :photo, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
