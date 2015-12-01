cba83320b2bc901bfeb426c0e764edc651e42fa6
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-12-01 17:33:36 -0300

    Relation inter-model coupon_location

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,8 +1,11 @@
 class Coupon < ActiveRecord::Base
-	belongs_to :user
-	validates :title, presence: true
-	validates :description, presence: true
-	validates :photo, presence: true
-	validates :value, presence: true
-	validates :expiration, presence: true
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :value, presence: true
+  validates :expiration, presence: true
+
+  has_many :coupon_locations
+  has_many :locations, through: :coupon_locations
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ActiveRecord::Base
+  has_many :coupon_locations
+  has_many :coupons, through: :coupon_locations
 end
