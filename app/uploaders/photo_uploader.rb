4e88c53176ad5776daecb821be9f74a7477eb10a
Alfredo Lobos <alfredo.lobos.g@gmail.com>
2015-12-04 23:47:17 -0300

    Fix photo to coupon, make fit

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -25,7 +25,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [1000, 1000]
+  process :resize_to_fit => [1000, 1000]
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +33,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [50, 50]
+    process :resize_to_fit => [50, 50]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
